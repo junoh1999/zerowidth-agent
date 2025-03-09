54a2171d648d3ee0983c24a8de3558f47ff1c8ca
junoh1999 <157660909+junoh1999@users.noreply.github.com>
2025-03-08 16:56:18 -0800

    asd

--- pages/index.js
+++ pages/index.js
@@ -334,69 +334,35 @@ export default function AgentComponent() {
                 }
               }}
             />
-                      <button
-            type="submit"
-            aria-label="Send prompt"
-            data-testid="send-button"
-            disabled={isLoading}
-            onMouseOver={() => setIsSubmitHovered(true)}
-            onMouseOut={() => setIsSubmitHovered(false)}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: "9999px",
-              transition: "opacity 0.2s ease",
-              backgroundColor: isSubmitHovered ? "#007BFF" : "#000",
-              color: isSubmitHovered ? "#fff" : "#fff",
-              height: "36px",
-              width: "36px",
-              border: "5px solid #fff",
-              cursor: isLoading ? "default" : "pointer",
-            }}
-          >
-            {!isLoading ? (
-              <svg
-                width="36px"
-                height="36px"
-                viewBox="8 8 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M15.1918 8.90615C15.6381 8.45983 16.3618 8.45983 16.8081 8.90615L21.9509 14.049C22.3972 14.4953 22.3972 15.2189 21.9509 15.6652C21.5046 16.1116 20.781 16.1116 20.3347 15.6652L17.1428 12.4734V22.2857C17.1428 22.9169 16.6311 23.4286 15.9999 23.4286C15.3688 23.4286 14.8571 22.9169 14.8571 22.2857V12.4734L11.6652 15.6652C11.2189 16.1116 10.4953 16.1116 10.049 15.6652C9.60265 15.2189 9.60265 14.4953 10.049 14.049L15.1918 8.90615Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            ) : (
-              <svg
-                width="36px"
-                height="36px"
-                viewBox="0 0 50 50"
-                style={{ animation: "spin 1s linear infinite" }}
-              >
-                <circle
-                  cx="25"
-                  cy="25"
-                  r="20"
-                  stroke="#888"
-                  strokeWidth="12"
-                  fill="none"
-                />
-                <circle
-                  cx="25"
-                  cy="25"
-                  r="20"
-                  stroke="#fff"
-                  strokeWidth="12"
-                  strokeDasharray="31.4 31.4"
-                  fill="none"
-                />
-              </svg>
-            )}
-          </button>
+<div 
+  onClick={handleSubmit}
+  style={{
+    width: "40px",
+    height: "40px", 
+    backgroundColor: "#FFFFFF",
+    borderRadius: "50%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    position: "absolute",
+    bottom: "-20px",
+    left: "20px",
+  }}
+>
+  {isLoading ? (
+    <div className="loading-spinner" style={{ width: "20px", height: "20px" }} />
+  ) : (
+    <img 
+      src="/arrow_upward.svg" 
+      alt="Submit" 
+      style={{ 
+        width: "20px", 
+        height: "20px" 
+      }} 
+    />
+  )}
+</div>
           </form>
         </div>
       </div>
